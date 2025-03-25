38265b9f38c67eda8bceec380eddb6fa7123e332
Rejwanul Islam <rejwanulislam45@gmail.com>
2025-03-25 23:44:17 +0600

    lockel stroge setup

--- src/pajes/CoffeeDetails.jsx
+++ src/pajes/CoffeeDetails.jsx
@@ -1,32 +1,56 @@
 import React, { useEffect, useState } from 'react'
 import { useLoaderData, useParams } from 'react-router-dom'
+import { addFavorite, getAllFavorite } from '../utlis'
 
 export default function CoffeeDetails() {
   const { id } = useParams()
   const data = useLoaderData()
   console.log(data)
   const [coffee, setcoffee] = useState({})
-  const {  image,rating,popularity, category,type, ingredients, nutrition_info, calories, fat, carbohydrates, protein ,name} = coffee
+  const { image, rating, popularity, category, type, ingredients, nutrition_info, calories, fat, carbohydrates, protein, name } = coffee
+
+  const handleAddCoffee = (coffee) => {
+    addFavorite(coffee)
+    setisFavorite(true)
+
+
+  }
+  const [isFavorite, setisFavorite] = useState(false)
 
   useEffect(
     () => {
       const singleData = data.find(coffee => (coffee.id == id))
       setcoffee(singleData)
+      const favorits = getAllFavorite()
+
+      const isExist = favorits.find(item => item.id == singleData.id)
+      console.log(isExist)
+      if (isExist) {
+        setisFavorite(true)
+      }
     }, [id, data]
 
   )
 
   return (
-    <div className="card w-full bg-base-100 shadow-xl ">
-    <figure><img src={image} alt="Shoes" /></figure>
-    <div className="card-body">
-      <h2 className="card-title">Coffee Name:{name}</h2>
-      <p>Category:{category}</p>
-      <p>type:{type} </p>
-      <p>rating:{rating} </p>
-      <p>popularity:{popularity} </p>
-      
+    <div>
+      <div className="card w-full bg-base-100 shadow-xl ">
+        <figure><img src={image} alt="Shoes" /></figure>
+        <div className='flex'>
+          <div className="card-body">
+            <h2 className="card-title">Coffee Name:{name}</h2>
+            <p>Category:{category}</p>
+            <p>type:{type} </p>
+            <p>rating:{rating} </p>
+            <p>popularity:{popularity} </p>
+
+          </div>
+          <div>
+            <button disabled={isFavorite} onClick={() => handleAddCoffee(coffee)} className='btn btn-success w-32'>Add Favaraute</button>
+          </div>
+        </div>
+      </div>
+
     </div>
-  </div>
   )
 }
---
+++ src/utlis/index.js
@@ -0,0 +1,31 @@
+//get all data lockel stroge
+const getAllFavorite = () => {
+    const all = localStorage.getItem('favorits')
+    if (all) {
+        const favorits = JSON.parse(all)
+        console.log(favorits)
+        return favorits
+    }
+    else {
+        console.log([])
+        return []
+    }
+
+
+}
+
+
+//add coffee to lockel stroge
+const addFavorite = (coffee) => {
+    console.log(coffee)
+    const favorits = getAllFavorite();
+    const isExist = favorits.find(item=> (item.id == coffee.id))
+    if(isExist)return alert('allrady exist')
+    console.log(favorits)
+    favorits.push(coffee)
+    localStorage.setItem('favorits', JSON.stringify(favorits))
+}
+
+
+
+export { addFavorite, getAllFavorite };
